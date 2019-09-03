8fa897ea5f17e482d436d29371bc98381cdee350
dongxuanliang <dongxl123@163.com>
2019-09-03 18:00:21 +0800

    UrlExtractFunction

---
+++ src/main/java/com/winbaoxian/common/freemarker/functions/UrlExtractFunction.java
@@ -0,0 +1,71 @@
+package com.winbaoxian.common.freemarker.functions;
+
+import freemarker.template.TemplateMethodModelEx;
+import freemarker.template.TemplateModel;
+import freemarker.template.TemplateModelException;
+import freemarker.template.utility.DeepUnwrap;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UrlExtractFunction implements TemplateMethodModelEx {
+
+    /**
+     * USAGE
+     * urlExtract(url).getPath()
+     * urlExtract(url).getParameter(key)
+     *
+     * @param list
+     * @return
+     * @throws TemplateModelException
+     */
+    @Override
+    public Object exec(List list) throws TemplateModelException {
+        if (CollectionUtils.isNotEmpty(list)) {
+            String urlStr = (String) DeepUnwrap.unwrap((TemplateModel) list.get(0));
+            return new UrlExtractor(urlStr);
+        }
+        return null;
+    }
+
+    public static class UrlExtractor {
+
+        private String path;
+        private Map<String, String> parameters = new HashMap<>();
+
+        public UrlExtractor(String url) {
+            if (StringUtils.isBlank(url)) {
+                return;
+            }
+            String[] urlParts = StringUtils.split(url, "?");
+            this.path = urlParts[0];
+            if (urlParts.length < 2) {
+                return;
+            }
+            String[] parameterParts = StringUtils.split(urlParts[1], "&");
+            if (parameterParts.length < 1) {
+                return;
+            }
+            for (String parameterPart : parameterParts) {
+                String[] ps = StringUtils.split(parameterPart, "=");
+                if (ps.length < 2) {
+                    continue;
+                }
+                parameters.put(StringUtils.trim(ps[0]), StringUtils.trim(ps[1]));
+            }
+        }
+
+        public String getPath() {
+            return this.path;
+        }
+
+        public String getParameter(String key) {
+            return parameters.get(key);
+        }
+
+    }
+
+}
