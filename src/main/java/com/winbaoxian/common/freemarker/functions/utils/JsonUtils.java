e991607505bc777e1e1216b96fed030c31a76245
dongxuanliang <dongxl123@163.com>
2019-12-03 11:36:38 +0800

    JsonUtils升级

--- src/main/java/com/winbaoxian/common/freemarker/functions/utils/JsonUtils.java
+++ src/main/java/com/winbaoxian/common/freemarker/functions/utils/JsonUtils.java
@@ -4,7 +4,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.bson.types.ObjectId;
+
+import java.util.Date;
 
 /**
  * @author dongxuanliang252
@@ -19,18 +22,23 @@ public enum JsonUtils {
     private final static String OPEN_BRACKET = "[";
     private final static String CLOSE_BRACKET = "]";
 
+    private final String JSON_DATE_FORMAT_PATTERN = "'date'(yyyy-MM-dd HH:mm:ss.S)";
+
     public Object parseObject(String text) {
         if (StringUtils.isBlank(text)) {
             return null;
         }
         text = StringUtils.trim(text);
-        if (StringUtils.startsWith(text, OPEN_BRACE) && StringUtils.endsWith(text, CLOSE_BRACE)) {
-            return JSON.parseObject(text);
-        } else if (StringUtils.startsWith(text, OPEN_BRACKET) && StringUtils.endsWith(text, CLOSE_BRACKET)) {
-            return JSON.parseArray(text);
-        } else {
+        try {
+            if (StringUtils.startsWith(text, OPEN_BRACE) && StringUtils.endsWith(text, CLOSE_BRACE)) {
+                return JSON.parseObject(text);
+            } else if (StringUtils.startsWith(text, OPEN_BRACKET) && StringUtils.endsWith(text, CLOSE_BRACKET)) {
+                return JSON.parseArray(text);
+            }
+        } catch (Exception e) {
             return text;
         }
+        return text;
     }
 
     public String toJSONString(Object o) {
@@ -43,8 +51,11 @@ public enum JsonUtils {
         public Object process(Object o, String filedName, Object filedValue) {
             if (filedValue instanceof ObjectId) {
                 return ((ObjectId) filedValue).toString();
+            } else if (filedValue instanceof Date) {
+                return DateFormatUtils.format((Date) filedValue, JSON_DATE_FORMAT_PATTERN);
             }
             return filedValue;
         }
     }
+
 }
