300897d90b1a7ac622511c0fbaf80f3823fd594c
dongxuanliang <dongxl123@163.com>
2019-12-20 14:00:56 +0800

    增加freemarker配置抽象类

---
+++ src/main/java/com/winbaoxian/common/freemarker/AbstractFreemarkerConfiguration.java
@@ -0,0 +1,56 @@
+package com.winbaoxian.common.freemarker;
+
+import com.winbaoxian.common.freemarker.functions.*;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+
+public abstract class AbstractFreemarkerConfiguration {
+
+    protected void settings(Configuration configuration) throws TemplateException {
+         commonSettings(configuration);
+         customSettings(configuration);
+    }
+
+    /**
+     * 扩展配置
+     * @param configuration
+     */
+    protected abstract void customSettings(Configuration configuration) throws TemplateException;
+
+    /**
+     * 通用配置
+     * @param configuration
+     * @throws TemplateException
+     */
+    private void commonSettings(Configuration configuration) throws TemplateException {
+        configuration.setSetting(Configuration.NUMBER_FORMAT_KEY, "0.######;;roundingMode=halfUp");
+        configuration.setSetting(Configuration.DATE_FORMAT_KEY, "yyyy-MM-dd");
+        configuration.setSetting(Configuration.TIME_FORMAT_KEY, "HH:mm:ss");
+        configuration.setSetting(Configuration.DATETIME_FORMAT_KEY, "yyyy-MM-dd HH:mm:ss");
+        configuration.setSharedVariable("random", new RandomFunction());
+        configuration.setSharedVariable("range", new RangeFunction());
+        configuration.setSharedVariable("toJSONString", new ToJSONStringFunction());
+        configuration.setSharedVariable("toJSON", new ToJSONFunction());
+        configuration.setSharedVariable("toCookieString", new ToCookieStringFunction());
+        configuration.setSharedVariable("descartes", new DescartesFunction());
+        configuration.setSharedVariable("repeat", new RepeatFunction());
+        configuration.setSharedVariable("matchNumber", new MatchNumberFunction());
+        configuration.setSharedVariable("extractTagValues", new ExtractTagValuesFunction());
+        configuration.setSharedVariable("getBirthday", new GetBirthdayFunction());
+        configuration.setSharedVariable("escapeString", new EscapeStringFunction());
+        configuration.setSharedVariable("jsoup", new JsoupFunction());
+        configuration.setSharedVariable("jsoupXpath", new JsoupXpathFunction());
+        configuration.setSharedVariable("generateIdNum", new GenerateIdNumFunction());
+        configuration.setSharedVariable("makeDateByDelta", new MakeDateByDeltaFunction());
+        configuration.setSharedVariable("aes", new AESFunction());
+        configuration.setSharedVariable("base64", new Base64Function());
+        configuration.setSharedVariable("md5", new Md5Function());
+        configuration.setSharedVariable("hmac", new HmacFunction());
+        configuration.setSharedVariable("tripleDES", new TripleDESFunction());
+        configuration.setSharedVariable("urlExtract", new UrlExtractFunction());
+        configuration.setSharedVariable("jsonPathExtract", new JsonPathExtractFunction());
+        configuration.setSharedVariable("listByKey", new ListByKeyFunction());
+        configuration.setSharedVariable("round", new RoundFunction());
+    }
+
+}
